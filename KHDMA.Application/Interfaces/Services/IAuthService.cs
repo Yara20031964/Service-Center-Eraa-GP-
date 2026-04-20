050c7dacdcd5c2c974d7bf0f1d0e4dd87d349c90
Sharawelly <lordmarawanshrief@gmail.com>
2026-04-20 12:03:26 +0200

    commit

---
+++ KHDMA.Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using KHDMA.Application.DTOs.Auth.Request;
+using KHDMA.Application.DTOs.Auth.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KHDMA.Application.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        Task<AuthResponseDto> RegisterCustomerAsync(RegisterCustomerDto registerDto);
+        Task<AuthResponseDto> RegisterProviderAsync(RegisterProviderDto registerDto);
+        Task<AuthResponseDto> LoginAsync(LoginDto loginDto);
+        Task<AuthResponseDto> RefreshTokenAsync(string refreshToken);
+        Task <bool> RevokeTokenAsync(string refreshToken);
+    }
+}
